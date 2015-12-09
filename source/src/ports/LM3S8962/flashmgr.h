83547763d9cb5a73c0d71cf39735bc80932e3bb3
capxilinx <melik-merkumians@acin.tuwien.ac.at>
2015-12-09 13:14:45 +0100

    Closes #40 Removes LM3S8962 support

--- source/src/ports/LM3S8962/flashmgr.h
+++
@@ -1,8 +0,0 @@
-/*******************************************************************************
- * Copyright (c) 2009, Rockwell Automation, Inc.
- * All rights reserved.
- *
- ******************************************************************************/
-
-extern unsigned long *findNextEmptyParameterBlock(void);
-extern void writeParameter(unsigned long *data, int dataSize);
