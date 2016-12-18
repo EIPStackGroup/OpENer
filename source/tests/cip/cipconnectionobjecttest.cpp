a944536663f2e95d9d75234b53902de5e7a2a5ae
CapXilinx <melik-merkumians@acin.tuwien.ac.at>
2016-12-18 22:12:42 +0100

    Adds test files for CipConnectionObject

---
+++ source/tests/cip/cipconnectionobjecttest.cpp
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2016, Rockwell Automation, Inc.
+ * All rights reserved.
+ *
+ ******************************************************************************/
+
+#include <CppUTest/TestHarness.h>
+#include <stdint.h>
+#include <string.h>
+
+extern "C" {
+
+#include "cipconnectionobject.h"
+
+}
+
+TEST_GROUP(CipConnectionObject) {
+
+};
