6df345a13e1196cfb9b776abedb433c7f412ea8c
CapXilinx <melik-merkumians@acin.tuwien.ac.at>
2018-01-30 09:33:52 +0100

    Adds assert that Attribute 13 is available
    Signed-off-by: CapXilinx <melik-merkumians@acin.tuwien.ac.at>

--- source/src/cip/ciptcpipinterface.c
+++ source/src/cip/ciptcpipinterface.c
@@ -384,6 +384,7 @@ EipStatus GetAttributeAllTcpIpInterface(
 
 EipUint16 GetEncapsulationInactivityTimeout(CipInstance *instance) {
   CipAttributeStruct *attribute = GetCipAttribute(instance, 13);
+  OPENER_ASSERT(NULL != attribute);
   CipUint *data = (CipUint *) attribute->data;
   EipUint16 encapsulation_inactivity_timeout = *data;
   return encapsulation_inactivity_timeout;
