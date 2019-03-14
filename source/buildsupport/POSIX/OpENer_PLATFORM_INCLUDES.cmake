722cb2e1ff2ad21dfcb31a33c28a79a1dae8e36a
Bill McCormick <wmccormick@swri.org>
2019-03-14 16:17:37 -0500

    using CMAKE_* build vars

--- source/buildsupport/POSIX/OpENer_PLATFORM_INCLUDES.cmake
+++ source/buildsupport/POSIX/OpENer_PLATFORM_INCLUDES.cmake
@@ -1,6 +1,8 @@
 macro(opener_platform_spec)
   include_directories(${PORTS_SRC_DIR}/${OpENer_PLATFORM} ${PORTS_SRC_DIR}/${OpENer_PLATFORM}/sample_application)
   set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99 -Werror=implicit-function-declaration" )
+  set( CMAKE_INSTALL_LIBDIR "lib")
+  set( CMAKE_INSTALL_INCLUDEDIR "include")
   add_definitions( -D_POSIX_C_SOURCE=200112L -D__USE_GNU -D__USE_XOPEN2K -DOPENER_POSIX)
   get_property(languages GLOBAL PROPERTY ENABLED_LANGUAGES)
   if ("CXX" IN_LIST languages)
