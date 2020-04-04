fa4f6c534ce2d0fc5c06a5c0f7d472231e179741
Jason Valenzuela <jvalenzuela1977@gmail.com>
2020-04-04 18:37:50 -0400

    Create platform wrapper functions for send/recv functions.
    These wrappers are intended to resolve parameter and return value data
    type differences between Windows and POSIX, i.e. ssize_t, size_t, and
    int. See comments for the new functions in networkhandler.h for details.
    This resolves Visual Studio warnings about implicit conversions from
    size_t to int or long.
    
    A wrapper for recvfrom() was not created in this commit as it currently
    does not generate Visual Studio warnings, however, it should probably
    get similar treatment.

--- source/src/ports/MINGW/networkhandler.c
+++ source/src/ports/MINGW/networkhandler.c
@@ -14,6 +14,13 @@
 
 #include "generic_networkhandler.h"
 
+
+static EipStatus SocketResult(
+   const int result,
+   size_t *const bytes_xfr
+);
+
+
 MicroSeconds getMicroSeconds() {
   LARGE_INTEGER performance_counter;
   LARGE_INTEGER performance_frequency;
@@ -57,4 +64,112 @@ int SetQosOnSocket(socket_platform_t socket,
    *  in the IP header, must be shifted left 2 bits. */
   DWORD set_tos = qos_value << 2;
   return setsockopt(socket, IPPROTO_IP, IP_TOS, (char *)&set_tos, sizeof(set_tos) );
+}
+
+
+/*
+ * This wrapper needs to convert the two length parameters from size_t
+ * to int, and uses the Windows SOCKET_ERROR constant to detect success
+ * or failure.
+ */
+EipStatus SendToPlatform(
+   const socket_platform_t socket,
+   const void *const buf,
+   const size_t buf_len,
+   const int flags,
+   const struct sockaddr *const dest_addr,
+   const socklen_t dest_len,
+   size_t *const bytes_sent
+) {
+   /* Sanity check for parameters cast to a different type. */
+   OPENER_ASSERT(buf_len <= INT_MAX);
+   OPENER_ASSERT(dest_len <= INT_MAX);
+
+   const int result = sendto(
+      socket,
+      buf,
+      (int)buf_len,
+      flags,
+      dest_addr,
+      (int)dest_len
+   );
+
+   return SocketResult(result, bytes_sent);
+}
+
+
+/*
+ * This wrapper needs to convert the length parameter from size_t
+ * to int, and uses the Windows SOCKET_ERROR constant to detect success
+ * or failure.
+ */
+EipStatus SendPlatform(
+   const socket_platform_t socket,
+   const void *const buf,
+   const size_t buf_len,
+   const int flags,
+   size_t *const bytes_sent
+) {
+   /* Sanity check for parameters cast to a different type. */
+   OPENER_ASSERT(buf_len <= INT_MAX);
+
+   const int result = send(
+      socket,
+      buf,
+      (int)buf_len,
+      flags
+   );
+
+   return SocketResult(result, bytes_sent);
+}
+
+
+/*
+ * This wrapper needs to convert the length parameter from size_t to int,
+ * and use the Windows SOCKET_ERROR constant to detect success or failure.
+ */
+EipStatus RecvPlatform(
+   const socket_platform_t socket,
+   void *const buf,
+   const size_t buf_len,
+   const int flags,
+   size_t *const bytes_received
+) {
+   /* Sanity check for parameters cast to a different type. */
+   OPENER_ASSERT(buf_len <= INT_MAX);
+
+   const int result = recv(
+      socket,
+      buf,
+      (int)buf_len,
+      flags
+   );
+
+   return SocketResult(result, bytes_received);
+}
+
+
+/** @brief Clean up function to handle send/recv results.
+ *
+ * @param result Return value from the send/recv call.
+ * @param bytes_xfr Location to store the number of bytes transferred
+ *                  if the underlying socket call was successful.
+ *
+ * @return kEipStatusOk or kEipStatusError.
+ */
+static EipStatus SocketResult(
+   const int result,
+   size_t *const bytes_xfr
+) {
+   /* Convert the return value to an EIP status constant. */
+   const EipStatus status =
+     (result == SOCKET_ERROR) ? kEipStatusError : kEipStatusOk;
+
+   /* Update the number of bytes sent if successful. */
+   if ((status == kEipStatusOk) && (bytes_xfr != NULL)) {
+      OPENER_ASSERT(result <= MAXSIZE_T);
+      *bytes_xfr = (size_t)result;
+   }
+
+   return status;
 }
--- source/src/ports/POSIX/networkhandler.c
+++ source/src/ports/POSIX/networkhandler.c
@@ -19,6 +19,13 @@
 #include "encap.h"
 #include "opener_user_conf.h"
 
+
+static EipStatus SocketResult(
+   const int result,
+   size_t *const bytes_xfr
+);
+
+
 MicroSeconds GetMicroSeconds(void) {
   struct timespec now = { .tv_nsec = 0, .tv_sec = 0 };
 
@@ -67,4 +74,99 @@ int SetQosOnSocket(const socket_platform_t socket,
    *  in the IP header, must be shifted left 2 bits. */
   int set_tos = qos_value << 2;
   return setsockopt(socket, IPPROTO_IP, IP_TOS, &set_tos, sizeof(set_tos) );
+}
+
+
+/*
+ * This wrapper only needs to translate the return value into one of
+ * the EIP status constants.
+ */
+EipStatus SendToPlatform(
+   const socket_platform_t socket,
+   const void *const buf,
+   const size_t buf_len,
+   const int flags,
+   const struct sockaddr *const dest_addr,
+   const socklen_t dest_len,
+   size_t *const bytes_sent
+) {
+   const ssize_t result = sendto(
+      socket,
+      buf,
+      buf_len,
+      flags,
+      dest_addr,
+      dest_len
+   );
+
+   return SocketResult(result, bytes_sent);
+}
+
+
+/*
+ * This wrapper only needs to translate the return value into one of
+ * the EIP status constants.
+ */
+EipStatus SendPlatform(
+   const socket_platform_t socket,
+   const void *const buf,
+   const size_t buf_len,
+   const int flags,
+   size_t *const bytes_sent
+) {
+   const int result = send(
+      socket,
+      buf,
+      buf_len,
+      flags
+   );
+
+   return SocketResult(result, bytes_sent);
+}
+
+
+/*
+ * This wrapper only needs to translate the return value into one of
+ * the EIP status constants.
+ */
+EipStatus RecvPlatform(
+   const socket_platform_t socket,
+   void *const buf,
+   const size_t buf_len,
+   const int flags,
+   size_t *const bytes_received
+) {
+   const int result = recv(
+      socket,
+      buf,
+      buf_len,
+      flags
+   );
+
+   return SocketResult(result, bytes_received);
+}
+
+
+/** @brief Clean up function to handle send/recv results.
+ *
+ * @param result Return value from the send/recv call.
+ * @param bytes_sent Location to store the number of bytes transferred
+ *                   if the underlying socket call was successful.
+ *
+ * @return kEipStatusOk or kEipStatusError.
+ */
+static EipStatus SocketResult(
+   const int result,
+   size_t *const bytes_xfr
+) {
+   /* Convert the return value to an EIP status constant. */
+  const EipStatus status = (result == -1) ? kEipStatusError : kEipStatusOk;
+
+  /* Update the number of bytes sent if successful. */
+   if ((status == kEipStatusOk) && (bytes_xfr != NULL)) {
+      OPENER_ASSERT(result <= MAXSIZE_T);
+      *bytes_xfr = (size_t)result;
+   }
+
+   return status;
 }
