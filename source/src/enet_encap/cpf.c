476c7b0e99beed9a2c83e4ab9d7c55de408820bf
Jason Valenzuela <jvalenzuela1977@gmail.com>
2020-04-03 20:11:33 -0400

    Change variable name to avoid masking other variable with the same name.

--- source/src/enet_encap/cpf.c
+++ source/src/enet_encap/cpf.c
@@ -174,7 +174,7 @@ EipStatus NotifyConnectedCommonPacketFormat(
                &g_common_packet_format_data_item,
                outgoing_message);
 
-            CipOctet *buffer = outgoing_message->current_message_position;
+            CipOctet *msg_pos = outgoing_message->current_message_position;
             outgoing_message->current_message_position =
               outgoing_message->message_buffer;
             GenerateEncapsulationHeader(received_data,
@@ -182,7 +182,7 @@ EipStatus NotifyConnectedCommonPacketFormat(
                                         received_data->session_handle,
                                         kEncapsulationProtocolSuccess,
                                         outgoing_message);
-            outgoing_message->current_message_position = buffer;
+            outgoing_message->current_message_position = msg_pos;
             memcpy(&connection_object->last_reply_sent,
                    outgoing_message,
                    sizeof(ENIPMessage) );
