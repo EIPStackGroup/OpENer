d6ee68da932891d12948a00456ba4567bbb35057
Jason Valenzuela <jvalenzuela1977@gmail.com>
2022-01-07 17:00:34 -0500

    Update successful return constant documentation per f68e049.

--- source/src/cip/cipioconnection.c
+++ source/src/cip/cipioconnection.c
@@ -264,7 +264,7 @@ EipUint16 SetupIoConnectionTargetToOriginatorConnectionPoint(
  * @param connection_object pointer to the connection object structure holding the parsed data from the forward open request
  * @param extended_error the extended error code in case an error happened
  * @return general status on the establishment
- *    - kEipStatusOk ... on success
+ *    - kCipErrorSuccess ... on success
  *    - On an error the general status code to be put into the response
  */
 CipError EstablishIoConnection(
