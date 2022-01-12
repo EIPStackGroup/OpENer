0f7d02471463a4a9ad4ff6b89d4b869a33cc78d5
Jason Valenzuela <jvalenzuela1977@gmail.com>
2022-01-11 19:15:18 -0500

    Remove unnecessary local data pointer variables.
    These pointers generated compiler warnings about differing const
    qualifiers, e.g. Visual Studio C4090, due to a mismatch in the level of
    pointer indirection. In other words, the local variable was a pointer to
    a pointer, which was assigned the value(not address of) a pointer.

--- source/src/cip/cipassembly.c
+++ source/src/cip/cipassembly.c
@@ -156,9 +156,6 @@ int DecodeCipAssemblyAttribute3(CipByteArray *const data,
                                 CipMessageRouterRequest *const message_router_request,
                                 CipMessageRouterResponse *const message_router_response)
 {
-
-  const EipUint8 **const cip_message = message_router_request->data;
-
   CipInstance *const instance =
     GetCipInstance(GetCipClass(
                      message_router_request->request_path.class_id),
@@ -182,7 +179,9 @@ int DecodeCipAssemblyAttribute3(CipByteArray *const data,
   }
 
   // data-length is correct
-  memcpy(cip_byte_array->data, cip_message, cip_byte_array->length);
+  memcpy(cip_byte_array->data,
+         message_router_request->data,
+         cip_byte_array->length);
 
   if(AfterAssemblyDataReceived(instance) != kEipStatusOk) {
     /* punt early without updating the status... though I don't know
