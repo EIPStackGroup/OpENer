e58cc348b6debf4c83990571caff3b407f00af7f
micsat <michael.satovich@tuwien.ac.at>
2022-07-22 12:49:21 +0200

    uncrustify update

--- source/src/cip/cipcommon.c
+++ source/src/cip/cipcommon.c
@@ -152,7 +152,7 @@ EipStatus NotifyClass(const CipClass *RESTRICT const cip_class,
   return kEipStatusOkSend;
 }
 
-CipUint GetMaxInstanceNumber(CipClass *RESTRICT const cip_class){
+CipUint GetMaxInstanceNumber(CipClass *RESTRICT const cip_class) {
   CipUint max_instance = 0;
   CipInstance *instance = cip_class->instances;
   while (NULL != instance) { /* loop trough all instances of class */
@@ -257,7 +257,7 @@ CipInstance *AddCipInstance(CipClass *RESTRICT const cip_class,
     instance = AddCipInstances(cip_class, 1);
     instance->instance_number = instance_id;
   }
- 
+
   cip_class->max_instance = GetMaxInstanceNumber(cip_class); /* update largest instance number (class Attribute 2) */
 
   return instance;
