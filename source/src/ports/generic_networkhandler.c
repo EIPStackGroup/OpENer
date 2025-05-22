eaaa0673bd1d3f5f663cc8e637668f6737b05098
Ivan <ivanxiaojia@outlook.com>
2025-05-22 17:42:55 +0800

    move error_message further down.

--- source/src/ports/generic_networkhandler.c
+++ source/src/ports/generic_networkhandler.c
@@ -1090,12 +1090,12 @@ void CheckAndHandleConsumingUdpSocket(void) {
       }
 
       if(0 > received_size) {
+        if(OPENER_SOCKET_WOULD_BLOCK == error_code) {
+          return; // No fatal error, resume execution
+        } 
         int error_code = GetSocketErrorNumber();
         char *error_message = GetErrorMessage(error_code);
-        if(OPENER_SOCKET_WOULD_BLOCK == error_code) {
-          FreeErrorMessage(error_message);
-          return; // No fatal error, resume execution
-        } OPENER_TRACE_ERR("networkhandler: error on recv: %d - %s\n",
+        OPENER_TRACE_ERR("networkhandler: error on recv: %d - %s\n",
                            error_code,
                            error_message);
         FreeErrorMessage(error_message);
