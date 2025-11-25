27cafb4743eb89741cd59d80227531ba8197b370
Martin Melik Merkumians <martin.melik@gmail.com>
2025-11-25 16:11:42 +0100

    remove legacy CI/CD files

--- travis_scripts/windowsMinGWScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'Windows MinGW main Script started...'
-# Exit with nonzero exit code if anything fails
-set -e
-
-cd $TRAVIS_BUILD_DIR/source
-cmake -G "MinGW Makefiles" -DOpENer_PLATFORM:STRING="MINGW" -DCMAKE_BUILD_TYPE:STRING="Debug" -DCMAKE_SH="CMAKE_SH-NOTFOUND" .
-mingw32-make
--- travis_scripts/windowsScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Windows main Script started...'
-# Exit with nonzero exit code if anything fails
-set -e
-
-cd $TRAVIS_BUILD_DIR/source
-cmake -DOpENer_PLATFORM:STRING="WIN32" -DCMAKE_BUILD_TYPE:STRING="Debug" .
-"c:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" OpENer.sln //p:Configuration=Release //p:Platform="Win32"
